d8a71f7ad32ccb4a6362e74a12cc75f923626820
Magd El Zalameh <Magd El Zalameh>
2025-07-31 22:27:25 +0300

    Header: Background, Title and Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Search from "./assets/components/search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState("");
     return (
-        <div>App</div>
+        <main>
+            <div className='patter'/>
+
+            <div className='wrapper'>
+                <header>
+                    <img src='./hero.png' alt='Hero Banner' />
+                    <h1>Find <span className='text-gradient'>movies</span> that you'll enjoy!</h1>
+                </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+        </main>
     )
 }
 export default App
